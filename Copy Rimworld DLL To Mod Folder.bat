4beebf8707c27adc4cd739005d6628081c0776e8
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-08-05 03:58:49 +0100

    Create Copy Rimworld DLL To Mod Folder.bat

---
+++ Copy Rimworld DLL To Mod Folder.bat
@@ -0,0 +1,7 @@
+REM Change folders as required
+xcopy "C:\Users\cdoherty\Documents\GitHub\HoldOpenOpensDoors\Assemblies\Hold_Open_Opens_Doors.dll" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Hold Open Opens Doors\Assemblies\" /Y
+xcopy "C:\Users\cdoherty\Documents\GitHub\HoldOpenOpensDoors\Source\obj\Debug\Hold_Open_Opens_Doors.pdb" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Hold Open Opens Doors\Assemblies\" /Y
+xcopy "C:\Users\cdoherty\Documents\GitHub\HoldOpenOpensDoors\About\About.xml" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Hold Open Opens Doors\About\" /Y
+xcopy "C:\Users\cdoherty\Documents\GitHub\HoldOpenOpensDoors\About\Manifest.xml" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Hold Open Opens Doors\About\" /Y
+xcopy "C:\Users\cdoherty\Documents\GitHub\HoldOpenOpensDoors\About\Preview.png" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Hold Open Opens Doors\About\" /Y
+xcopy "C:\Users\cdoherty\Documents\GitHub\HoldOpenOpensDoors\About\PublishedFileId.txt" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\Hold Open Opens Doors\About\" /Y
